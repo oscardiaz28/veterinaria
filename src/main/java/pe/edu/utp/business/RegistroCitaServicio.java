d5aab541464d20683daa60091fb092c6e627e888
Eberth <125581299+EberthSN@users.noreply.github.com>
2024-07-16 10:48:50 -0500

    Haciendo commit de listar cita servico por FabricioSantaCruz

--- src/main/java/pe/edu/utp/business/RegistroCitaServicio.java
+++ src/main/java/pe/edu/utp/business/RegistroCitaServicio.java
@@ -4,10 +4,12 @@ import pe.edu.utp.model.CitaServicio;
 import pe.edu.utp.service.CitaServicioService;
 import pe.edu.utp.util.AppConfig;
 import pe.edu.utp.util.DataAccessMariaDB;
+import pe.edu.utp.utils.TextUTP;
 
 import javax.naming.NamingException;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.List;
 
 public class RegistroCitaServicio {
 
@@ -33,5 +35,36 @@ public class RegistroCitaServicio {
             throw new RuntimeException(e);
         }
     }
+
+    public String getHTMListarCitaServicios() throws IOException, SQLException {
+    // Cargar plantilla principal
+    String filename = "src\\main\\resources\\web\\cita_servicio.html";
+    String html = TextUTP.read(filename);
+
+    // Cargar plantilla para los items
+    String filenameItems = "src\\main\\resources\\templates\\listado_cita_servicio.html";
+    String htmlItem = TextUTP.read(filenameItems);
+
+    // Recorrer la lista
+    StringBuilder itemsHtml = new StringBuilder();
+
+    // Listar cita servicios
+    List<CitaServicio> listado = citaServicioService.getAllCitaServicios();
+
+    for (CitaServicio citaServicio : listado) {
+        // Reemplazar los marcadores en la plantilla del item
+        String item = htmlItem.replace("${id}", Integer.toString(citaServicio.getId()))
+                .replace("${cita_id}", Integer.toString(citaServicio.getCita_id()))
+                .replace("${servicio_id}", Integer.toString(citaServicio.getServicio_id()))
+                .replace("${estado}", citaServicio.getEstado());
+        itemsHtml.append(item);
+    }
+
+    // Reemplazar en la plantilla principal
+    String reporteHtml = html.replace("${itemsCitaServicio}", itemsHtml.toString());
+
+    return reporteHtml;
+}
+
 
 }
--- src/main/java/pe/edu/utp/model/CitaServicio.java
+++ src/main/java/pe/edu/utp/model/CitaServicio.java
@@ -1,17 +1,31 @@
 package pe.edu.utp.model;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter
 @Setter
 @ToString
-@NoArgsConstructor
 public class CitaServicio {
     private Integer id;
     private Integer cita_id;
     private Integer servicio_id;
     private String estado;
+
+    
+
+    public CitaServicio() {
+    }
+
+
+
+    public CitaServicio(Integer id, Integer cita_id, Integer servicio_id, String estado) {
+        this.id = id;
+        this.cita_id = cita_id;
+        this.servicio_id = servicio_id;
+        this.estado = estado;
+    }
+
+ 
 }
